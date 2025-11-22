2f2fb562fddcab08eb3fdd8bffa4a64582611e4a
Nathan Neidigh <nathan.neidigh@outlook.com>
2025-11-22 15:07:32 -0800

    Fixed lsp completions

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,8 +1,5 @@
 return {
 	{
-		"hrsh7th/cmp-nvim-lsp",
-	},
-	{
 		"L3MON4D3/LuaSnip",
 		dependencies = {
 			"saadparwaiz1/cmp_luasnip",
@@ -11,8 +8,15 @@ return {
 	},
 	{
 		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-cmdline",
+		},
 		config = function()
 			local cmp = require("cmp")
+			local luasnip = require("luasnip")
 			require("luasnip.loaders.from_vscode").lazy_load()
 
 			cmp.setup({
@@ -32,6 +36,24 @@ return {
 					["<C-Space>"] = cmp.mapping.complete(),
 					["<C-e>"] = cmp.mapping.abort(),
 					["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+					["<Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item()
+						elseif luasnip.expand_or_jumpable() then
+							luasnip.expand_or_jump()
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+					["<S-Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_prev_item()
+						elseif luasnip.jumpable(-1) then
+							luasnip.jump(-1)
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
 				}),
 				sources = cmp.config.sources({
 					{ name = "nvim_lsp" },
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -17,16 +17,19 @@ return {
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
 			vim.lsp.config("lua_ls", {
 				settings = {
 					Lua = {
 						diagnostics = { globals = { "vim" } },
 					},
 				},
+				capabilities = capabilities,
 			})
-			-- vim.lsp.config("pyright")
-			-- vim.lsp.config("clangd")
-			-- vim.lsp.config("verible")
+			vim.lsp.config("pyright", { capabilities = capabilities })
+			vim.lsp.config("clangd", { capabilities = capabilities })
+			vim.lsp.config("verible", { capabilities = capabilities })
 
 			vim.lsp.enable("lua_ls")
 			vim.lsp.enable("pyright")
